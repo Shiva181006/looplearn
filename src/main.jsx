6d661f07b943f1f5fa826aa48429954ec1f579d2
Shiva181006 <shivakasaudhan817@gmail.com>
2026-06-23 19:08:53 +0530

    Configure GitHub Pages deployment and update app routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import App from './App.jsx';
 import { ProblemProvider } from './context/ProblemContext.jsx';
 // import './styles.scss';
@@ -8,10 +8,10 @@ import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ProblemProvider>
         <App />
       </ProblemProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
